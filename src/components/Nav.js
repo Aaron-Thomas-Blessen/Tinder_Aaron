4fc95ed1eabe96b885162747d83ef69accd8c3a7
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-03-17 18:19:09 +0530

    hello

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import whitelogo from "../images/tinder_logo_white.png";
+import colorLogo from "../images/color-logo-tinder.png";
+
+const Nav = ({ minimal, authToken }) => {
+  return (
+    <nav>
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={minimal ? colorLogo : whitelogo}
+          alt="logo of tinder"
+        />
+      </div>
+      {!authToken && <button className="nav-button">Log in</button>}
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+  const authToken = false;
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <>
+      <Nav minimal={true} authToken={authToken} />
+      <div className="home">
+        <h1>Swipe Right</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Signout" : "Create Account"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
