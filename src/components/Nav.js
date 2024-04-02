c32221cf075d495e96b25b5f62b5fa076c32b611
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-04-02 23:05:57 +0530

    last commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,31 @@
-import whitelogo from "../images/tinder_logo_white.png";
+import whiteLogo from "../images/tinder_logo_white.png";
 import colorLogo from "../images/color-logo-tinder.png";
 
-const Nav = ({ minimal, authToken }) => {
+const Nav = ({ minimal, setShowModal, showModal, setIsSignUp }) => {
+  const handleClick = () => {
+    setShowModal(true);
+    setIsSignUp(false);
+  };
+  const authToken = true;
   return (
     <nav>
       <div className="logo-container">
         <img
           className="logo"
-          src={minimal ? colorLogo : whitelogo}
-          alt="logo of tinder"
+          src={minimal ? colorLogo : whiteLogo}
+          alt="logo"
         />
       </div>
-      {!authToken && !minimal && <button className="nav-button">Log in</button>}
+      {!authToken && !minimal && (
+        <button
+          className="nav-button"
+          onClick={handleClick}
+          disabled={showModal}
+        >
+          Log in
+        </button>
+      )}
     </nav>
   );
 };
-
 export default Nav;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,67 @@
-const Dashboard = () => {
-    return (
-        <div>Dashboard</div>
-    )
-}
+import TinderCard from "react-tinder-card";
+import { useEffect, useState } from "react";
 
-export default Dashboard
+const Dashboard = () => {
+  const characters = [
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg",
+    },
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg",
+    },
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg",
+    },
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg",
+    },
+    {
+      name: "Richard Hendricks",
+      url: "https://i.imgur.com/Q9WPlWA.jpeg",
+    },
+  ];
+
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing:" + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + "left the screen");
+  };
+
+  return (
+    <>
+      <div className="dashboard">
+        {/* {<ChatContainer user={user} />} */}
+        <div className="swipe-container">
+          <div className="card-container">
+            {characters.map((character) => (
+              <TinderCard
+                className="swipe"
+                key={character.name}
+                onSwipe={(dir) => swiped(dir, character.name)}
+                onCardLeftScreen={() => outOfFrame(character.name)}
+              >
+                <div
+                  style={{ backgroundImage: `url(${character.url})` }} // Use template literals for correct interpolation
+                  className="card"
+                >
+                  <h3>{character.name}</h3>
+                </div>
+              </TinderCard>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
